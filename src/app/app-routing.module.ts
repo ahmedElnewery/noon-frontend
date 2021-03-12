454d4b62bf4dea3b3efd81a86cc5f7c64209647c
ahmed mahmoud <ahmedm.elnewery@gmail.com>
2021-03-12 20:02:31 +0200

    making routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { ProductCartComponent } from './+product/product-cart/product-cart/product-cart.component';
 import { ProductDetailsComponent } from './+product/product-details/product-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:"productdetails",component:ProductDetailsComponent}
+  {path:"productdetails",component:ProductDetailsComponent},
+  {path:"",component:ProductCartComponent}
 ];
 
 @NgModule({
