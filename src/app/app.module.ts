8d259a58dc1d0d397974fe96d294844170a0093e
peter kameel <peterAlpert@github.com>
2021-03-13 05:53:17 +0200

    main page with header, footer and slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductCartComponent } from './+product/product-cart/product-cart/product-cart.component';
+import { ProductRateComponent } from './+shared-UI/product-rate/product-rate.component';
+import { HeaderComponent } from './+layout/header/header.component';
+import { ProductSlidshowComponent } from './+product/product-slidshow/product-slidshow.component';
+import { ProductCategoryComponent } from './+product/product-category/product-category.component';
+import { FooterComponent } from './+layout/footer/footer.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
       TestComponent,
-      ProductCartComponent
+      ProductCartComponent,
+      ProductRateComponent,
+      HeaderComponent,
+      ProductSlidshowComponent,
+      ProductCategoryComponent,
+      FooterComponent
    ],
   imports: [
     BrowserModule,
