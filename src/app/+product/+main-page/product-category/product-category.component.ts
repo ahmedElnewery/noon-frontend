94ace6723f149ecd0ddb87fee76095f2edba5f9b
salma335 <engsalma335@gmail.com>
2021-03-28 04:22:14 +0200

    routing2

--- src/app/+product/+main-page/product-category/product-category.component.ts
+++ src/app/+product/+main-page/product-category/product-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-category',
@@ -8,19 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class ProductCategoryComponent implements OnInit {
 
   categoryImg = [
-    { src: "../../../assets/Img/test/en_category-01.png" },
-    { src: "../../../assets/Img/test/en_category-02.png" },
-    { src: "../../../assets/Img/test/en_mb-category-10.png" },
-    { src: "../../../assets/Img/test/en_category-04.png" },
-    { src: "../../../assets/Img/test/en_mb-category-01.png" },
-    { src: "../../../assets/Img/test/en_mb-category-02.png" },
-    { src: "../../../assets/Img/test/en_mb-category-03.png" },
-    { src: "../../../assets/Img/test/en_mb-category-05.png" },
-    { src: "../../../assets/Img/test/en_mb-category-07.png" },
-    { src: "../../../assets/Img/test/en_mb-category-09.png" },
+    { src: "../../../assets/Img/test/en_category-01.png" ,category:"menWear"},
+    { src: "../../../assets/Img/test/en_category-02.png" ,category:"womenWear"},
+    { src: "../../../assets/Img/test/en_mb-category-10.png" ,category:"kidWear"},
+    { src: "../../../assets/Img/test/en_category-04.png" ,category:"kitchen"},
+    { src: "../../../assets/Img/test/en_mb-category-01.png" ,category:"mobiles"},
+    { src: "../../../assets/Img/test/en_mb-category-02.png" ,category:"labtops"},
+    { src: "../../../assets/Img/test/en_mb-category-03.png" ,category:"tv"},
+    { src: "../../../assets/Img/test/en_mb-category-05.png" ,category:"kitchen"},
+    { src: "../../../assets/Img/test/en_mb-category-07.png" ,category:"makeup"},
+    { src: "../../../assets/Img/test/en_mb-category-09.png" ,category:"hairCare"},
   ]
-  constructor() { }
-
+  constructor(private router:Router) { }
+  GoToThisCategory(item){
+    this.router.navigate(['/filter', item.category])
+  }
   ngOnInit(): void {
   }
 
